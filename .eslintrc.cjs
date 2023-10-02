d83d7a6a50eee492adfcd2288f0e5c4a5941b9b7
Abel Lopes <abelflopes@gmail.com>
2023-10-02 21:07:28 +0100

    chore: update centralised eslint config version

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,46 +1,6 @@
 /** @type import("eslint").Linter.Config */
 const config = {
   extends: "@abelflopes/eslint-config-tsr-pro",
-  rules: {
-    // prefer component arrow functions
-    "react/function-component-definition": [
-      1,
-      {
-        namedComponents: "arrow-function",
-        unnamedComponents: "arrow-function",
-      },
-    ],
-    // ignore to allow usage of tsconfig aliases
-    "import/no-unresolved": 1,
-    // allow abbreviations
-    "unicorn/prevent-abbreviations": [
-      1,
-      {
-        checkProperties: true,
-        allowList: {
-          db: true,
-          param: true,
-          params: true,
-          props: true,
-          Props: true,
-        },
-      },
-    ],
-    "@typescript-eslint/strict-boolean-expressions": 1,
-    "unicorn/filename-case": [
-      1,
-      {
-        cases: {
-          kebabCase: true,
-          pascalCase: true,
-        },
-      },
-    ],
-    "react/button-has-type": 1,
-    "no-implicit-coercion": 1,
-    "unicorn/no-array-reduce": 1,
-    "unicorn/no-array-for-each": 1,
-  },
 };
 
 module.exports = config;
